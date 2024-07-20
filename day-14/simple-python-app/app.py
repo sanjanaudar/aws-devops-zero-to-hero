b0db92440d905dc87fa19fc53c09931039e41549
Sanjana Udar <sanjana.udar@gmail.com>
2024-07-20 15:58:17 -0400

    Update app.py - testing code pipeline again

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,10 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!'
+    return 'Hello, world! - Testing code pipeline'
 
 if __name__ == '__main__':
     app.run()
-
 
 
