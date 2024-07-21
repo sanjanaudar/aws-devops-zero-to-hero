88fa48956644b7a87f7e1a5d8bfcda73f21c2c7b
Sanjana Udar <sanjana.udar@gmail.com>
2024-07-20 22:45:02 -0400

    Update start_container.sh s3 access

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -4,7 +4,7 @@ set -e
 # Pull the Docker image from Docker Hub
 docker push sanjanaudar/simple-python-flask-app
 
-#trying build artifact config in CodePipeline
+#trying build artifact config in CodePipeline and added s3 access to codeBuild
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 sanjanaudar/simple-python-flask-app
