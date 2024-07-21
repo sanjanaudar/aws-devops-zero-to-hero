c9ee1992115c58bffaa09841f3b69dc147865cd3
Sanjana Udar <sanjana.udar@gmail.com>
2024-07-20 21:43:02 -0400

    Update start_container.sh testing CD

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -3,6 +3,7 @@ set -e
 
 # Pull the Docker image from Docker Hub
 docker pull sanjanaudar/simple-python-flask-app
+echo "Testing code deploy"
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 sanjanaudar/simple-python-flask-app
